75c094fa1a8baa2c69ff0781b53e5b02cd747ab3
Micha Koutjakin <kutyakinma@modeltech.ru>
2021-02-17 23:30:15 +0300

    init. Copied from simulator

---
+++ block.js
@@ -0,0 +1,111 @@
+class Block {
+  static EVENTS = {
+    INIT: 'init',
+    FLOW_CDM: 'flow:component-did-mount',
+    FLOW_RENDER: 'flow:render',
+  };
+
+  _element = null;
+  _meta = null;
+
+  /** JSDoc
+   * @param {string} tagName
+   * @param {Object} props
+   *
+   * @returns {void}
+   */
+  constructor(tagName = 'div', props = {}) {
+    const eventBus = new EventBus();
+    this._meta = {
+      tagName,
+      props,
+    };
+
+    this.props = this._makePropsProxy(props);
+
+    this.eventBus = () => eventBus;
+
+    this._registerEvents(eventBus);
+    eventBus.emit(Block.EVENTS.INIT);
+  }
+
+  _registerEvents(eventBus) {
+    eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+  }
+
+  _createResources() {
+    const {tagName} = this._meta;
+    this._element = this._createDocumentElement(tagName);
+  }
+
+  init() {
+    this._createResources();
+  }
+
+  _componentDidMount() {
+    this.componentDidMount();
+  }
+
+  // Может переопределять пользователь, необязательно трогать
+  componentDidMount(oldProps) {
+  }
+
+  _componentDidUpdate(oldProps, newProps) {
+    const response = this.componentDidUpdate(oldProps, newProps);
+  }
+
+  // Может переопределять пользователь, необязательно трогать
+  componentDidUpdate(oldProps, newProps) {
+    return true;
+  }
+
+  setProps = nextProps => {
+    if (!nextProps) {
+      return;
+    }
+
+    Object.assign(this.props, nextProps);
+  };
+
+  get element() {
+    return this._element;
+  }
+
+  _render() {
+    const block = this.render();
+    // Этот небезопасный метод для упрощения логики
+    // Используйте шаблонизатор из npm или напишите свой безопасный
+    // Нужно не в строку компилировать (или делать это правильно),
+    // либо сразу в DOM-элементы возвращать из compile DOM-ноду
+    this._element.innerHTML = block;
+  }
+
+  // Может переопределять пользователь, необязательно трогать
+  render() {
+  }
+
+  getContent() {
+    return this.element;
+  }
+
+  _makePropsProxy(props) {
+    // Можно и так передать this
+    // Такой способ больше не применяется с приходом ES6+
+    const self = this;
+
+    return props;
+  }
+
+  _createDocumentElement(tagName) {
+    // Можно сделать метод, который через фрагменты в цикле создаёт сразу несколько блоков
+    return document.createElement(tagName);
+  }
+
+  show() {
+  }
+
+  hide() {
+  }
+}
